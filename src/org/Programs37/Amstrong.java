416ab86d6076f17d50e6134808ec3f9ee3e216d9
ramyamuthusaravanan <ramyamca2@gmail.com>
2022-07-24 22:10:53 +0530

    rm_1

--- src/org/Programs37/Amstrong.java
+++ src/org/Programs37/Amstrong.java
@@ -5,21 +5,20 @@ import java.util.Scanner;
 public class Amstrong {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to check wheater it is amstong number or not?");
 		int n = sc.nextInt();
-		int a,i=0,j=0;
-		a=n;
-		while(a>0) {
-		i=a%10;
-		j=(i*i*i)+j;
-		a=a/10;
+		int a, i = 0, j = 0;
+		a = n;
+		while (a > 0) {
+			i = a % 10;
+			j = (i * i * i) + j;
+			a = a / 10;
 		}
-		if(n==j) {
-		System.out.println("It is amstrong");
-		}
-		else {
-		System.out.println("It is not a amstrong");
+		if (n == j) {
+			System.out.println("It is amstrong");
+		} else {
+			System.out.println("It is not a amstrong");
 		}
 
 	}
